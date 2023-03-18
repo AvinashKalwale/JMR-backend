83d9a3a1e16bb9bbe0b7f33d68fe45ff1649b383
Avinash Kalwale <avinashkalwale12@gmail.com>
2023-03-18 08:25:10 +0530

    backend updated with required functions

---
+++ index.js
@@ -0,0 +1,17 @@
+let dotenv=require('dotenv').config()
+const express = require("express");
+const app = express();
+const port =process.env.PORT || 5000
+const formRoutes = require("./src/routes/crud");
+const loginRoutes= require("./src/routes/login");
+require("./src/connectionAndSchema/config");
+let cors = require("cors")
+
+ 
+app.use(cors())
+app.use("/product",formRoutes)
+app.use("/",loginRoutes)
+
+app.listen(port,() => console.log(`App listening on port ${port}!`));
+
+module.exports = app;
